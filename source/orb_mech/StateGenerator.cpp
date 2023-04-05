872a58d63b510144d3a8512d30aec14e3add6004
Alex Shaver <alexshaver@gmail.com>
2023-04-05 16:37:20 -0500

    Start sketching in EccAnom helper
    This can't be directly solved, but a newtonian solver is
    pretty decent at this problem, esp. since we know
    the derivative of the method directly.
    
    Signed-off-by: Alex Shaver <alexshaver@gmail.com>

--- source/orb_mech/StateGenerator.cpp
+++ source/orb_mech/StateGenerator.cpp
@@ -1,9 +1,54 @@
 #include "StateGenerator.h"
 
+#include <functional>
+
 namespace orb_mech{
+
+namespace {
+// simple Newtonian solver; since we know derivative a priori, this is easy to use
+double findFunctionRoot(const std::function<double(double)>& f,
+                        const std::function<double(double)>& derivative,
+                        double x0,
+                        double tolerance = 1e-6);
+
+// the formula for eccentric anomaly arranged to = 0 for root finding
+double eccentricAnomaly_f(double meanAnomaly, double eccentricity, double eccentricAnomaly);
+// the derivative of eccentricAnomaly_f wrt eccentricAnomaly
+double eccentricAnomaly_d(double eccentricity, double eccentricAnomaly);
+
+} // namespace
 
 StateVector generateStateAtEpoch(const OrbitalElements& elements){
   return {{{},{},{}}, {{}, {}, {}}};
 }
+
+Angle eccentricAnomaly(Angle meanAnomaly, double eccentricity){
+  return Angle::Zero();
+}
+
+namespace {
+
+double findFunctionRoot(const std::function<double(double)>& f,
+                        const std::function<double(double)>& derivative,
+                        double x0,
+                        double tolerance) {
+  double x{x0}, lastX, limit;
+  do {
+    lastX = x;
+    x = x - f(x) / derivative(x);
+    limit = (0==lastX) ? tolerance : tolerance*lastX;
+  } while(limit < std::fabs(lastX - x));
+  return x;
+}
+
+double eccentricAnomaly_f(double meanAnomaly, double eccentricity, double eccentricAnomaly){
+  return eccentricAnomaly - eccentricity*std::sin(eccentricAnomaly) - meanAnomaly;
+}
+
+double eccentricAnomaly_d(double eccentricity, double eccentricAnomaly){
+  return 1 - eccentricity*std::cos(eccentricAnomaly);
+}
+
+} // namespace
 
 } // namespace orb_mech
--- source/orb_mech/StateGenerator.h
+++ source/orb_mech/StateGenerator.h
@@ -9,4 +9,8 @@ namespace orb_mech{
 /// the valuable function, in the future, will be generating state at a non-zero time
 StateVector generateStateAtEpoch(const OrbitalElements& elements);
 
+/// helper function to find eccentric anomaly given mean anomaly and eccentricity
+/// This does not have a simple algebraic answer, so this requires some effort
+Angle eccentricAnomaly(Angle meanAnomaly, double eccentricity);
+
 } // namespace orb_mech
