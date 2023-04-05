ef5928ad6e9b05bb9d81b0234b70fffa98203ab0
Alex Shaver <alexshaver@gmail.com>
2023-04-05 10:42:54 -0500

    Analyze more physical data set
    Starting with orbits that are more physical (i.e. something with a period of
    O(1e4) seconds, distances of 1e6, etc.) We see a much smaller drift in radius,
    worst case is like 8mm over 1Mm over 100 seconds (with approximately an error
    growing cubically). Also, in terms of time to generate, O(100 sec) to generate
    100 sec of data at 1µs precision is reasonable to generate 24 test cases in
    40 minutes (approximately).
    So now we can just move forward with generating data sets for our example
    problems (which we have yet to define)
    
    Signed-off-by: Alex Shaver <alexshaver@gmail.com>

--- utilities/orbit_generator/data_generator.py
+++ utilities/orbit_generator/data_generator.py
@@ -23,25 +23,27 @@ def build_table(
     col_index = pd.MultiIndex.from_product(col_head, names=["vector", "component"])
 
     df = pd.DataFrame.from_dict(test_data, orient="index", columns=col_index)
+    df["rad"] = df.apply(lambda row: np.hypot(row.position.x, row.position.y), axis=1)
     name = f"ts_{int(1000*timing_data.timestep_ms)}"
     df.to_hdf(output_file, name)
 
 
-def generate_true_data() -> pd.DataFrame:
+def generate_true_data(radius, speed, time_stop) -> pd.DataFrame:
     # generate 12 orbits at 8sec*50hz=400 intervals per orbit
     _time_sec = 0
     _test_data = {}
-    for orbit in range(12):
-        _angle = 0
-        for interval in range(400):
-            position = np.array([4 * np.cos(_angle), 4 * np.sin(_angle), 0])
-            velocity = np.array([-np.pi * np.sin(_angle), np.pi * np.cos(_angle), 0])
-            _test_data[_time_sec] = (*position, *velocity)
-            _angle = _angle + np.pi / 200
-            _time_sec = _time_sec + 0.02
+    _angle = 0
+    _angular_velocity = speed / radius
+    _angle_delta = _angular_velocity * 0.02  # 20 ms timestep
+    while _time_sec < time_stop:
+        position = np.array([radius * np.cos(_angle), radius * np.sin(_angle), 0])
+        velocity = np.array([-speed * np.sin(_angle), speed * np.cos(_angle), 0])
+        _test_data[_time_sec] = (*position, *velocity)
+        _angle = _angle + _angle_delta
+        _time_sec = _time_sec + 0.02
 
     # add last data point, which should be back at 0
-    _test_data[_time_sec] = (*np.array([4, 0, 0]), *np.array([0, np.pi, 0]))
+    # _test_data[_time_sec] = (*np.array([4, 0, 0]), *np.array([0, np.pi, 0]))
 
     col_head = [["position", "velocity"], ["x", "y", "z"]]
     col_index = pd.MultiIndex.from_product(col_head, names=["vector", "component"])
@@ -50,46 +52,49 @@ def generate_true_data() -> pd.DataFrame:
     return df
 
 
-def generate_timings():
+def generate_timings(stop_time_sec):
     timestep_bases = [1, 2, 5]
     # timestep_magnitudes = [0.001, 0.01, 0.1, 1, 10]
     timestep_magnitudes = [0.01, 0.1, 1, 10]
 
     _timings = []
+    _stop_time_ms = stop_time_sec * 1000
     for mag in timestep_magnitudes:
         for base in timestep_bases:
             timestep = mag * base
-            # 96 = 8*12 = 8 orbits
             timing = newtonian_orbit.DataTiming(
-                timestep_ms=timestep, stop_time_ms=96000, output_timestep_ms=20
+                timestep_ms=timestep, stop_time_ms=_stop_time_ms, output_timestep_ms=20
             )
             _timings.append(timing)
     return _timings
 
 
 if __name__ == "__main__":
-    output_file_name = "circles.h5"
+    output_file_name = "physical_circles.h5"
 
-    # produces circle with period = 8 seconds
-    # scenario_8sec_circle = TestScenario(
-    #     4 * np.pi * np.pi, np.array([4, 0, 0]), np.array([0, np.pi, 0])
-    # )
-    _p = np.array([4, 0, 0])
-    _v = np.array([0, np.pi, 0])
+    # choose something semi-physical
+    _mu = 1e12
+    _rad = 1e6
+    _p = np.array([_rad, 0, 0])
+    _speed = np.sqrt(_mu / _rad)
+    _v = np.array([0, _speed, 0])
     bound_generator = partial(
         build_table,
-        mu=4 * np.pi * np.pi,
+        mu=1e12,
         init_pos=_p,
         init_vel=_v,
         output_file=output_file_name,
     )
 
-    true_df = generate_true_data()
+    _stop_time_sec = 100
+    true_df = generate_true_data(_rad, _speed, _stop_time_sec)
 
     true_df.to_hdf(output_file_name, "true_circle")
 
-    timings = generate_timings()
+    timings = generate_timings(_stop_time_sec)
 
+    start = time.time()
     with concurrent.futures.ThreadPoolExecutor(14) as executor:
         executor.map(bound_generator, timings)
-    print("data generated")
+    stop = time.time()
+    print(f"data generated - {stop-start} sec")
